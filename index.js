8567d3bb275f90699f3320a4209eff71d06085a0
Habibur Rahman <habibur88454@gmail.com>
2024-12-15 12:40:28 +0600

    delete using specific id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 require('dotenv').config()
 const port = process.env.PORT || 9000
 const app = express()
@@ -49,6 +49,13 @@ async function run() {
       const jobs = req.body
       const result = await jobsCollection.insertOne(jobs);
       res.send(result)
+    })
+    
+    app.delete('/job/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await jobsCollection.deleteOne(query);
+      res.send(result);
     })
 
 
